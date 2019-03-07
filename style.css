f23ca794334b8d134f4612fe9267bafa0b880812
caro <carolina.ferreira131@gmail.com>
2019-03-07 20:47:40 +0100

    projet fini

---
+++ style.css
@@ -0,0 +1,23 @@
+body
+{
+  line-height: 1.6;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 650px;
+  font-size: 18px;
+  color:#444;
+  padding-left: 10px;
+  padding-right: 10px;
+
+}
+
+.police
+{
+  font-family: monospace;
+}
+h1, h2, h3
+{
+  line-height: 1.2;
+}
